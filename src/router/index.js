271dfd31f08f943c214728854defce177854b341
daniel <daniel.song@zoho.com>
2018-06-23 17:10:47 +0800

    add menu

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Menu from '@/components/Menu'
+import Goods from '@/components/Goods'
 
 Vue.use(Router)
 
@@ -8,6 +10,17 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Menu',
+      component: Menu,
+      children: [
+        {
+          path: 'goods',
+          component: Goods 
+        }
+      ]
+    },
+    {
+      path: '/helloworld',
       name: 'HelloWorld',
       component: HelloWorld
     }
